e0a9db87db62d1af8b55cc2d8437dbd5bebd3a85
Anuraj kumar <102934270+anurajkumar23@users.noreply.github.com>
2024-04-07 17:18:12 +0530

    banquethallid created

--- src/app/BanquetHall/[BanquetlHallId]/page.tsx
+++ src/app/BanquetHall/[BanquetlHallId]/page.tsx
@@ -1,8 +1,13 @@
+import MainCardPage from '@/components/MainCard/page'
 import React from 'react'
 
-const page = () => {
+const page: React.FC = () => {
   return (
     <div>
+      <div className='px-12 py-6'>
+        {/* <h1 className='text-3xl font-medium mb-4'>Top Banquet Halls in Patna</h1> */}
+        <MainCardPage />
+      </div>
       
     </div>
   )
