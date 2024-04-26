b20b2e8a20616a7e822676284fd35f7c264c7dcc
Anuraj kumar <102934270+anurajkumar23@users.noreply.github.com>
2024-04-26 13:57:11 +0530

    photographers home page completed

--- src/components/photographers/card.tsx
+++ src/components/photographers/card.tsx
@@ -1,4 +1,4 @@
-import { MapPin } from 'lucide-react';
+import { Heart, MapPin } from 'lucide-react';
 import Image, { StaticImageData } from 'next/image';
 import React from 'react';
 
@@ -15,25 +15,29 @@ interface cardProps {
 const card: React.FC<cardProps> = ({ img, alt, title, description, Price }) => {
     return (
         <div className='flex cursor-pointer py-5 justify-center items-center '>
-        <div className='w-48 bg-white z-10  border border-purple-300 p-2 rounded-2xl hover:scale-105 hover:border-purple-400 hover:border-4 transition duration-300 hover:shadow-lg'>
-            <Image
-                src={img}
-                alt={alt}
-                width={500}
-                height={500}
-                className='object-cover w-48 h-44 cursor-pointer rounded-2xl'
-            />
-            <div className="mt-3">
-                <strong className='font-medium md:text-lg text-base'>{title}</strong>
+            <div className='w-48 bg-white z-10  border border-purple-300 p-2 rounded-2xl hover:scale-105 hover:border-purple-400 hover:border-4 transition duration-300 hover:shadow-lg'>
+                <Image
+                    src={img}
+                    alt={alt}
+                    width={500}
+                    height={500}
+                    className='object-cover w-48 h-44 cursor-pointer rounded-2xl'
+                />
+                <div className='absolute border rounded-full p-1 bg-white ml-1 top-[170px] hover:scale-105 heart-shadow'>
+                    <Heart className='text-red-400 fill-red-400 z-20' />
+                </div>
+
+                <div className="mt-3">
+                    <strong className='font-medium md:text-lg text-base'>{title}</strong>
+                </div>
+                <div className='flex justify-between pt-2'>
+                    <h1 className='text-gray-600 text-xs'>{description}</h1>
+                    <h1 className='text-xl font-bold'>₹{Price}</h1>
+                </div>
+                <hr className='h-px mt-8 bg-black border-0' />
+                <h1 className='mt-2 justify-center flex font-bold cursor-pointer'>ORDER</h1>
             </div>
-            <div className='flex justify-between pt-2'>
-                <h1 className='text-gray-600 text-xs'>{description}</h1>
-                <h1 className='text-xl font-bold'>₹{Price}</h1>
-            </div>
-            <hr className='h-px mt-8 bg-black border-0' />
-            <h1 className='mt-2 justify-center flex font-bold cursor-pointer'>ORDER</h1>
         </div>
-    </div>
     );
 };
 
