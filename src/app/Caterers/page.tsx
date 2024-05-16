b445ed96ff2abb30a852902faa295f6795022d1b
Anuraj kumar <102934270+anurajkumar23@users.noreply.github.com>
2024-05-16 13:32:25 +0530

    Caterering responsive

--- src/app/Caterers/page.tsx
+++ src/app/Caterers/page.tsx
@@ -1,5 +1,7 @@
 import CardPage from '@/components/Caterers/page'
 import React from 'react'
+import ChefImage from "../../../public/Confident-smiling-female-chef-holding-two-plates-cooked-food-in-kitchen.jpeg"
+import Image from 'next/image';
 
 const page = () => {
 
@@ -7,15 +9,42 @@ const page = () => {
     { color: 'bg-gradient-to-l from-indigo-400 to-purple-700', name: 'Starter' },
     { color: 'bg-gradient-to-r from-amber-500 to-pink-500', name: 'Basic' },
     { color: 'bg-gradient-to-r from-emerald-400 to-cyan-400', name: 'Deluxe' }
-];
+  ];
 
   return (
-    <div className='py-6 mx-8'>
-      <div className='md:grid grid-cols-3 gap-4 '>
-      {cards.map((card, index) => (
-                <CardPage key={index} color={card.color} name={card.name} />
-            ))}
+    <div className=''>
+ <div className="border rounded-md overflow-hidden relative w-full h-60">
+      <div className='w-full lg:w-3/4 absolute h-60 z-20 bg-gradient-to-r from-pink-400 to-red-400 rounded-r-full flex flex-col justify-center'>
+        <h1 className="lg:p-5 p-2 font-bold lg:text-2xl text-base text-white">Customize your food According to your Venue</h1>
+        <div className='lg:justify-end lg:mr-10 flex justify-center '>
+          <div className="bg-white p-4 rounded-lg w-1/2 text-center">
+            <h2 className="lg:text-xl text-base font-bold">Book your personal halwai</h2>
+            <p className="text-sm">Let our expert chefs take care of your event with delicious food.</p>
+            <button className="bg-indigo-500 text-white px-4 py-2 mt-4 rounded-md hover:bg-indigo-600 transition duration-300">Book Now</button>
+          </div>
         </div>
+      </div>
+
+      <div className='hidden lg:block w-2/6 lg:h-full absolute top-0 right-0 h-full'>
+        <Image
+          src={ChefImage}
+          alt="Chef"
+          className="object-cover h-full"
+          layout="responsive"
+          width={500}
+          height={500}
+        />
+      </div>
+
+      <div className="absolute hidden lg:bottom-14 top-2 left-0 z-30 p-5">
+        <p className="text-white text-sm font-semibold">*Enjoy personalized catering services for your special event</p>
+      </div>
+    </div>
+      <div className='mx-8 md:grid grid-cols-3 gap-4 py-4'>
+        {cards.map((card, index) => (
+          <CardPage key={index} color={card.color} name={card.name} />
+        ))}
+      </div>
     </div>
   )
 }
--- src/components/Caterers/page.tsx
+++ src/components/Caterers/page.tsx
@@ -1,34 +1,90 @@
-import React from 'react';
+"use client"
+import React, { useState } from 'react';
 import { Button } from '../ui/button';
 import Veg from "../../../public/Veg_symbol.svg"
 import NonVeg from "../../../public/Non_veg_symbol.svg"
 import Image from 'next/image';
-import Item from 'antd/es/list/Item';
+
+import {
+    AlertDialog,
+    AlertDialogTrigger,
+    AlertDialogContent,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogCancel,
+} from "@/components/ui/alert-dialog";
+// import { GoCheckCircleFill } from 'react-icons/go';
+import { IceCream, Martini, Sandwich, Soup } from 'lucide-react';
+// import { IceCreamBowl } from 'lucide-react';
 
 const CardPage = ({ color, name }: any) => {
 
-    const FoodItems=[
-        {name:'Welcome Drinks', items:2},
-        {name:'Starter', items:2},
-        {name:'Welcome Drinks', items:2},
-        {name:'Starter', items:2},
-        {name:'Main Courses', items:3},
-        {name:'Salad', items:2},
-        {name:'Rice/Biryani', items:1},
-        {name:'Deserts', items:2},
+    const FoodItems = [
+        { name: 'Welcome Drinks', items: 2 },
+        { name: 'Starter', items: 2 },
+        { name: 'Welcome Drinks', items: 2 },
+        { name: 'Starter', items: 2 },
+        { name: 'Main Courses', items: 3 },
+        { name: 'Salad', items: 2 },
+        { name: 'Rice/Biryani', items: 1 },
+        { name: 'Deserts', items: 2 },
     ]
+    const servicesData = [
+        {
+            category: 'Welcome Drinks',
+            items: ["Sprite", "CocaCola", "Mirinda", "Lemonade", "Iced Tea", "Fruit Punch"]
+        },
+        {
+            category: 'Starters',
+            items: ["Paneer Tikka", "Chicken Tikka", "Vegetable Spring Rolls", "Samosas", "Chicken Satay", "Bruschetta"]
+        },
+        {
+            category: 'Main Course',
+            items: ["Butter Chicken", "Veg Biryani", "Dal Makhani", "Tandoori Roti", "Chicken Curry", "Palak Paneer"]
+        },
+        {
+            category: 'Desserts',
+            items: ["Gulab Jamun", "Rasgulla", "Rasmalai", "Ice Cream", "Fruit Salad", "Kheer"]
+        },
+       
+    ];
 
+    const getIconForCategory = (category: string) => {
+        switch (category) {
+            case 'Welcome Drinks':
+                return <Martini size={25} className="text-blue-500" />;
+            case 'Attire':
+                return <Martini size={25} className="text-purple-500" />;
+            case 'Desserts':
+                return <IceCream size={25} className="text-red-500" />;
+            case 'Starters':
+                return <Sandwich size={25} className="text-yellow-500" />;
+            case 'Main Course':
+                return <Soup size={25} className="text-green-500" />;
+            default:
+                return null;
+        }
+    };
+
+
+    const [showAllServices, setShowAllServices] = useState(false);
+
+    const toggleShowAllServices = () => {
+        setShowAllServices(!showAllServices);
+    };
 
     return (
         <div className="border">
-            <div className={`mb-4 border p-2 rounded-sm ${color}`}>
+            <div className={`mb-4 border p-4 rounded-sm ${color}`}>
                 <div className='justify-center flex items-center'>
-                    <h1 className='text-white font-semibold'>{name}</h1>
+                    <h1 className='text-white font-bold text-2xl'>{name}</h1>
                 </div>
 
             </div>
             <div className='flex px-4 items-center justify-between pb-2 '>
-                <Button variant="secondary" className='gap-x-2'>
+                <Button variant="secondary" className='gap-x-2 bg-slate-200'>
                     <Image
                         src={Veg}
                         alt="Veg"
@@ -49,7 +105,7 @@ const CardPage = ({ color, name }: any) => {
             </div>
             <div className='border-t-4 rounded-sm border-green-600 p-2 '>
                 <div className='grid grid-cols-10'>
-                    <div className='p-2 col-span-6 '>
+                    <div className='p-4 col-span-6 '>
                         <h1 className='font-semibold pb-4'>Food Items</h1>
                         {FoodItems.map((item, index) => (
                             <div key={index} className='pb-2 text-sm '>
@@ -60,8 +116,8 @@ const CardPage = ({ color, name }: any) => {
                     <div className='col-span-4 text-center'>
                         <h1 className='font-semibold'>Packages</h1>
                         <div className='text-xs flex justify-center'>
-                        <h1 className='font-semibold pb-4'> ₹ 700 </h1>
-                        <h1>/Plate</h1>
+                            <h1 className='font-semibold pb-4'> ₹ 700 </h1>
+                            <h1>/Plate</h1>
                         </div>
                         {FoodItems.map((item, index) => (
                             <div key={index} className='pb-2 text-sm'>
@@ -69,6 +125,44 @@ const CardPage = ({ color, name }: any) => {
                             </div>
                         ))}
                     </div>
+                </div>
+                <div className='justify-center flex w-full '>
+                    <AlertDialog>
+                        <AlertDialogTrigger>
+                            <Button
+                                variant="outline"
+                                onClick={toggleShowAllServices}
+                                className="text-blue-500 mt-4 border-blue-500 "
+                            >
+                                See Menu items
+                            </Button>
+                        </AlertDialogTrigger>
+                        <AlertDialogContent >
+                            <AlertDialogHeader>
+                                <AlertDialogTitle className='justify-center flex place-center'>Food Items</AlertDialogTitle>
+                            </AlertDialogHeader>
+                            <AlertDialogDescription>
+                                <div className="grid grid-cols-2 mt-4">
+                                    {servicesData.map((service, index) => (
+                                        <div key={index} className="pb-4 flex items-center">
+                                            {getIconForCategory(service.category)}
+                                            <div>
+                                                <h1 className="font-semibold">{service.category}</h1>
+                                                <ul className="list-disc pl-6">
+                                                    {service.items.map((item, idx) => (
+                                                        <li key={idx}>{item}</li>
+                                                    ))}
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    ))}
+                                </div>
+                            </AlertDialogDescription>
+                            <AlertDialogFooter>
+                                <AlertDialogCancel>Close</AlertDialogCancel>
+                            </AlertDialogFooter>
+                        </AlertDialogContent>
+                    </AlertDialog>
                 </div>
             </div>
         </div>
