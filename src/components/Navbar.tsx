dc28cb8093e1b741fc9d2d69e2fe67ca1ed52f52
Anuraj kumar <102934270+anurajkumar23@users.noreply.github.com>
2024-08-13 10:42:52 +0530

    fix the sideNavbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,31 +4,20 @@ import Image from 'next/image';
 import Link from 'next/link';
 import logo from "../../public/Dream_Wedding_Logo.png"
 import { useState } from 'react';
-import { AiOutlineClose } from 'react-icons/ai';
-import { navLinks } from '@/lib/constants';
-import { LogOut } from 'lucide-react';
-import { useAuth } from '@/app/authContext';
-import { useRouter } from 'next/navigation';
 import { ShowLogin, ShowLogout } from '@/utils/protect/protect';
+import SideNavbar from './SideNavbar';
 
 
 const Navbar: React.FC = () => {
 
   const [menuOpen, setMenuOpen] = useState(false)
-  const { user, setUser } = useAuth();
 
-  const router = useRouter()
 
   const handleNav = () => {
     setMenuOpen(!menuOpen)
   }
 
-  function handlelogout(){
-    localStorage.clear()
-    setUser(null)
-    router.push("/")
-  
-  }
+ 
 
   return (
     <nav className=" z-50 w-full h-20 shadow-xl bg-slate-100 ">
@@ -70,7 +59,6 @@ const Navbar: React.FC = () => {
               </li>
             </Link>
             </ShowLogout>
-            {/* <Link href='/user/profile'> */}
             <ShowLogin>
             <li>
              
@@ -81,66 +69,11 @@ const Navbar: React.FC = () => {
               
             </li>
             </ShowLogin>
-            {/* </Link> */}
-          </ul>
-        </div>
-      </div>
-      {/* <div className='font-semibold bottom-0 w-full h-20 shadow-xl bg-slate-100  fixed md:hidden' style={{ boxShadow: '0px -4px 6px rgba(0, 0, 0, 0.1)' }}>
-        <ul className='flex justify-between items-center h-full text-sm px-3'>
-          <li>
-          <Link href="/">
-          <RiHome3Line className="text-2xl w-full items-center"/>
-          Home</Link></li>
-          <li>Banquet Halls</li>
-          <li>Caterer</li>
-          <li>Decorators</li>
-          <li>Photographers</li>
-        </ul>
-      </div> */}
-      <div className={`fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 backdrop-blur-sm bg-opacity-75 z-50 ${menuOpen ? 'opacity-100 visible' : 'opacity-0 invisible'}`} onClick={handleNav}>
-        <div className={`fixed right-0 top-0 w-[365px] h-full bg-slate-50  ease-in duration-500 ${menuOpen ? 'translate-x-0' : 'translate-x-full'}`} onClick={(e) => e.stopPropagation()}>
-          <div className='p-4 flex cursor-pointer items-center justify-start mb-4'>
-            <AiOutlineClose size={25} onClick={handleNav} />
-          </div>
-          <Link href="/user/profile">
-          <div className=" flex flex-col items-center text-center">
-            <Avatar>
-              <AvatarImage src="https://github.com/shadcn.png" />
-              <AvatarFallback>AN</AvatarFallback>
-            </Avatar>
-            <p className="mt-4 font-semibold text-lg">Anuraj</p>
-            <p className="text-blue-500 cursor-pointer">Click to view profile</p>
-          </div>
-          </Link>
-          <hr className="w-full border-t border-gray-300" />
-          <ul className="p-2 overflow-y-auto pb-60  h-full hide-scrollbar text-black font-medium">
-            {navLinks.map((link, index) => (
-              <div key={link.label}>
-                <Link href={link.url}
-                  // variant="outline"
-                  className="p-4 flex w-full justify-start space-x-2  hover:bg-gray-200 hover:rounded-md"
-                >
-                  {link.icon}
-                  <span>{link.label}</span>
-                </Link>
-                {index === 3 ||  index === 5 || index === 6 || index === 9 ? (
-                  <div className='mt-4 pb-4'>
-                    <hr className="w-full border-t border-gray-300" />
-                  </div>
-                ) : null}
-              </div>
-            ))}
-             <div  
-             onClick={handlelogout}            
-              className={`flex gap-4 p-4 text-body-medium hover:bg-red-100 hover:rounded-md  cursor-pointer "text-grey-1"
-                }`}
-            >
-              <LogOut/><p className="cursor-pointer">Logout</p>
-            </div>
-          </ul>
-        </div>
-      </div>
 
+          </ul>
+        </div>
+      </div>
+      <SideNavbar menuOpen={menuOpen} handleNav={handleNav}  />
     </nav>
   );
 };
---
+++ src/components/SideNavbar.tsx
@@ -0,0 +1,83 @@
+
+import { AiOutlineClose } from 'react-icons/ai';
+import { navLinks } from '@/lib/constants';
+import { LogOut } from 'lucide-react';
+import { useAuth } from '@/app/authContext';
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+
+
+interface SideNavbarProps {
+    menuOpen: boolean;
+    handleNav: () => void;
+}
+
+
+const SideNavbar = ({ menuOpen, handleNav }: any) => {
+    const { user, setUser } = useAuth();
+    const router = useRouter()
+
+
+    function handleLogout() {
+        localStorage.clear()
+        setUser(null)
+        router.push("/")
+        handleNav();
+    }
+
+
+
+    return (
+        <div>
+            <div className={`fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 backdrop-blur-sm bg-opacity-75 z-50 ${menuOpen ? 'opacity-100 visible' : 'opacity-0 invisible'}`} onClick={handleNav}>
+                <div className={`fixed right-0 top-0 w-[365px] h-full bg-slate-50  ease-in duration-500 ${menuOpen ? 'translate-x-0' : 'translate-x-full'}`} onClick={(e) => e.stopPropagation()}>
+                    <div className='p-4 flex cursor-pointer items-center justify-start mb-4'>
+                        <AiOutlineClose size={25} onClick={handleNav} />
+                    </div>
+                    <Link href="/user/profile"
+                        onClick={handleNav}
+                    >
+                        <div className=" flex flex-col items-center text-center">
+                            <Avatar>
+                                <AvatarImage src="https://github.com/shadcn.png" />
+                                <AvatarFallback>AN</AvatarFallback>
+                            </Avatar>
+                            <p className="mt-4 font-semibold text-lg">{user?.name}</p>
+                            <p className="text-blue-500 cursor-pointer">Click to view profile</p>
+                        </div>
+                    </Link>
+                    <hr className="w-full border-t border-gray-300" />
+                    <ul className="p-2 overflow-y-auto pb-60  h-full hide-scrollbar text-black font-medium">
+                        {navLinks.map((link, index) => (
+                            <div key={link.label}>
+                                <Link href={link.url}
+                                    // variant="outline"
+                                    className="p-4 flex w-full justify-start space-x-2  hover:bg-gray-200 hover:rounded-md"
+                                    onClick={handleNav}
+                                >
+                                    {link.icon}
+                                    <span>{link.label}</span>
+                                </Link>
+                                {index === 3 || index === 5 || index === 6 || index === 9 ? (
+                                    <div className='mt-4 pb-4'>
+                                        <hr className="w-full border-t border-gray-300" />
+                                    </div>
+                                ) : null}
+                            </div>
+                        ))}
+                        <div
+                            onClick={handleLogout}
+                            className={`flex gap-4 p-4 text-body-medium hover:bg-red-100 hover:rounded-md  cursor-pointer "text-grey-1"
+                }`}
+                        >
+                            <LogOut /><p className="cursor-pointer">Logout</p>
+                        </div>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SideNavbar
