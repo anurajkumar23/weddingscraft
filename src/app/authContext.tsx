f4ae19c8cf19eea246d9818e1223715fe8f13e64
Ishu Singh <104965112+ishuoncode@users.noreply.github.com>
2024-08-19 10:24:30 +0530

    modify login  add call in useauth

--- src/app/authContext.tsx
+++ src/app/authContext.tsx
@@ -1,23 +1,45 @@
-// contexts/AuthContext.tsx
-"use client";
-
+"use client"
 import { createContext, useContext, useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
 
 // Define the shape of your context
 interface AuthContextType {
   user: any;
   setUser: (user: any) => void;
-
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider = ({ children }: { children: any }) => {
   const [user, setUser] = useState<any>(null);
-  
+  const router = useRouter();
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      const id = localStorage.getItem("user_id");
+      if (!id) return; // Early return if no user_id
+
+      if (!user) {
+        console.log("entered the use useeffect")
+        try {
+          const response = await axios.get(
+            `${process.env.NEXT_PUBLIC_BACKEND_URL}/users/${id}`
+          );
+          console.log(response.data.data.user,"usedata from context")
+          setUser(response.data.data.user);
+ 
+        } catch (error) {
+          console.error("Error fetching user data:", error);
+        }
+      }
+    };
+
+    fetchUserData();
+  }, [user, router]); // Dependency array includes user and router
 
   return (
-    <AuthContext.Provider value={{ user, setUser}}>
+    <AuthContext.Provider value={{ user, setUser }}>
       {children}
     </AuthContext.Provider>
   );
@@ -26,8 +48,9 @@ export const AuthProvider = ({ children }: { children: any }) => {
 export const useAuth = () => {
   const context = useContext(AuthContext);
   if (context === undefined) {
-    throw new Error('useAuth must be used within an AuthProvider');
+    throw new Error("useAuth must be used within an AuthProvider");
   }
+  console.log("context",context)
 
   return context;
 };
