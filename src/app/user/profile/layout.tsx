d55e550ad7c060d43a32f36afd4b554f84c89a2f
Anuraj kumar <102934270+anurajkumar23@users.noreply.github.com>
2024-10-21 23:42:05 +0530

    add mobile device screen

--- src/app/user/profile/layout.tsx
+++ src/app/user/profile/layout.tsx
@@ -1,7 +1,8 @@
+"use client"
 // components/UserLayout.tsx
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect, useState } from 'react';
 import LeftSideBar from './LeftSideBar';
-import Navbar from '@/components/Navbar';
+
 
 
 
@@ -9,16 +10,34 @@ interface UserLayoutProps {
   children: ReactNode;
 }
 
-const UserLayout: React.FC<UserLayoutProps> = ({ children}) => {
+const UserLayout: React.FC<UserLayoutProps> = ({ children }) => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkIfMobile = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    checkIfMobile();
+    window.addEventListener('resize', checkIfMobile);
+
+    return () => {
+      window.removeEventListener('resize', checkIfMobile);
+    };
+  }, []);
+
   return (
-    <main className="flex">
-    <aside className="max-h-full sticky top-0 bottom-0 z-50">
-      <LeftSideBar/>
-    </aside>
-    <main className="w-full">
-      <nav className="w-full sticky top-0 z-40">{children}</nav>
+    <main className="relative flex flex-col md:flex-row">
+      <aside className={`${
+          isMobile ? 'fixed inset-y-0 left-0 z-50 ' : 'sticky top-0 max-h-full'
+        } transition-all duration-300 ease-in-out`}
+      >
+        <LeftSideBar />
+      </aside>
+      <main className="flex-grow">
+        <div className="p-4">{children}</div>
+      </main>
     </main>
-  </main>
   );
 };
 
